1116c4a4469c125aba198709954d38e462ff4858
flipmcf <flipmcf@gmail.com>
2022-04-21 11:35:04 -0400

    segments being created automatically now

--- vodhls.py
+++ vodhls.py
@@ -1,5 +1,6 @@
 import typing as t
 import os
+from os import path
 
 from config import config
 from bento4.mp4utils import Mp42Hls
@@ -7,7 +8,7 @@ from bento4.mp4utils import Mp42Hls
 import logging
 logger = logging.getLogger('vodhls')
 
-class Options_Config(object):
+class OptionsConfig(object):
     def __init__(self, config):
         self.base_config = config
 
@@ -15,8 +16,8 @@ class Options_Config(object):
         return self.base_config[item]
 
 
-def validate_path(path):
-    actualPath = '/'.join((config['videoParentPath'], path))
+def validate_path(dir_name):
+    actualPath: os.PathLike = os.path.join(config['videoParentPath'], dir_name)
 
     try:
         statinfo = os.stat(actualPath)
@@ -26,8 +27,8 @@ def validate_path(path):
     return True
 
 
-def manifest_exists(path: t.Union[os.PathLike, str]) -> bool:
-    childManifestPath = config['segmentParentPath']+path
+def manifest_exists(dir_name: t.Union[os.PathLike, str]) -> bool:
+    childManifestPath = config['segmentParentPath'] + dir_name
 
     try:
         statinfo = os.stat(childManifestPath)
@@ -36,16 +37,46 @@ def manifest_exists(path: t.Union[os.PathLike, str]) -> bool:
     return True
 
 
-def create_manifest_and_segments(path: t.Union[os.PathLike, str]) -> bool:
-    input_file: os.PathLike = '/'.join((config['videoParentPath'], path))
-    output_dir: os.PathLike = '/'.join((config['segmentParentPath'], path))
+def create_manifest_and_segments(dir_name: t.Union[os.PathLike, str]) -> bool:
+    input_file: os.PathLike = '/'.join((config['videoParentPath'], dir_name))
+    output_dir: os.PathLike = '/'.join((config['segmentParentPath'], dir_name))
     hls_config = {
             'exec_dir': "/home/mcfaddenm/Bento4/bin",
             'debug': True,
             'verbose': False,
-            'output_dir': output_dir,
             }
 
-    options = Options_Config(hls_config)
-    Mp42Hls(options, input_file)
+    kwargs = {
+        'index_filename':            path.join(output_dir, 'index_0_av.m3u8'),
+        'segment_filename_template': path.join(output_dir, 'segment-%d.ts'),
+        'segment_url_template':      'segment-%d.ts',
+        'show_info':                 True
+    }
+
+    options = OptionsConfig(hls_config)
+
+    if not os.path.isdir(output_dir):
+        make_segment_dir(output_dir)
+
+    try:
+        Mp42Hls(options, input_file, **kwargs)
+    except Exception:
+        logger.exception("Error creating segment files")
+        return False
+
+    return True
+
+
+def make_segment_dir(dir_name: t.Union[os.PathLike, str]) -> None:
+
+    if not os.path.isdir(config['segmentParentPath']):
+        # configuration is wrong.  This path should always exist.
+        logger.error("The segment directory doesn't exist")
+        raise FileNotFoundError
+
+    os.makedirs(path.join(config['segmentParentPath'], dir_name))
+
+
+
+
 
--- wsgi.py
+++ wsgi.py
@@ -27,18 +27,18 @@ def index():
            """
 
 
-@app.route('/i/<path:the_path>/index_0_av.m3u8')
-def child_manifest(the_path: t.Union[os.PathLike, str]):
-    if not vodhls.validate_path(the_path):
+@app.route('/i/<path:dir_name>/index_0_av.m3u8')
+def child_manifest(dir_name: t.Union[os.PathLike, str]):
+    if not vodhls.validate_path(dir_name):
         abort(404)
 
-    if not vodhls.manifest_exists(the_path + 'index_0_av.m3u8'):
-        app.logger.debug(f"child manifest for {the_path} does not exist")
-        vodhls.create_manifest_and_segments(the_path)
+    if not vodhls.manifest_exists(dir_name + 'index_0_av.m3u8'):
+        app.logger.debug(f"child manifest for {dir_name} does not exist")
+        vodhls.create_manifest_and_segments(dir_name)
 
-    app.logger.debug(f"returning {app.config['segmentParentPath']}/{the_path}/{app.config['childManifestFilename']}")
+    app.logger.debug(f"returning {app.config['segmentParentPath']}/{dir_name}/{app.config['childManifestFilename']}")
 
-    filepath = the_path + '/' + config['childManifestFilename']
+    filepath = dir_name + '/' + config['childManifestFilename']
 
     return send_from_directory(directory=app.config['segmentParentPath'],
                                path=filepath,
