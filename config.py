59e61855b59142935949076710692fe40c9cef89
flipmcf <flipmcf@gmail.com>
2022-04-25 14:23:38 -0400

    example config file.  Raise exception if config doesn't exist

--- config.py
+++ config.py
@@ -5,5 +5,10 @@ from configparser import ConfigParser
 
 def get_config() -> ConfigParser:
     config: ConfigParser = configparser.ConfigParser()
-    config.read('config.ini')
+    successfully_read = config.read('config.ini')
+
+    if 'config.ini' not in successfully_read:
+        raise FileNotFoundError('config file config.ini not found')
+
+
     return config
