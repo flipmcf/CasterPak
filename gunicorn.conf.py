a311e2c1ba3cbef5ccfc131ba4ec8f3daec4f968
flipmcf <flipmcf@gmail.com>
2022-05-19 13:10:00 -0400

    gunicorn config and simple systemd service

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+import applogging
+
+workers = 3
+bind = "unix:/home/casterpak/CasterPak/casterpak.sock"
+
+wsgi_app = "casterpak:app"
+umask = 777  #A bit mask for the file mode on files written by Gunicorn.
+
+loglevel = 'debug'
+accesslog = "/var/log/casterpak.access.log"
+errorlog = "/var/log/casterpak.error.log"
+logconfig_dict = applogging.CASTERPAK_DEFAULT_LOGGING_CONFIG
+
+capture_output = False
+
+
+wsgi_app = "casterpak:app"
