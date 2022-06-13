54b740dd6069d9ee710de96d45220b8f272039d7
flipmcf <flipmcf@gmail.com>
2022-06-13 16:13:36 -0400

    cleanup now takes care of input file cache

--- cleanup.py
+++ cleanup.py
@@ -16,21 +16,31 @@ class CacheCleaner(object):
     def __init__(self):
         self.output_dir = config['output']['segmentParentPath']
         self.output_filename = config['output']['childManifestFilename']
-        self.db = cachedb.CacheDB()
+        self.input_dir = config['input']['videoCachePath']
+        self.segment_db = cachedb.CacheDB(cache_name=cachedb.SEGMENT_FILE_CACHE)
+        self.input_file_db = cachedb.CacheDB(cache_name=cachedb.INPUT_FILE_CACHE)
 
     def clean(self):
-        files = self.get_old_files()
-        logger.debug(f"files to clean: {files}")
+        files = self.get_old_segment_files()
+        logger.debug(f"segment files to clean: {files}")
         for file in files:
-            self.delete(file)
+            self.delete_segment(file)
+
+        files = self.get_old_input_files()
+        logger.debug(f"input files to clean {files}")
+        for file in files:
+            self.delete_input(file)
 
         return 0
 
-    def get_old_files(self):
-        return self.db.find(int(config['cache']['segment_file_age']))
+    def get_old_segment_files(self):
+        return self.segment_db.find(int(config['cache']['segment_file_age']))
 
-    def delete(self, file):
-        logger.debug(f"asked to delete {file}")
+    def get_old_input_files(self):
+        return self.input_file_db.find(int(config['cache']['input_file_age']))
+
+    def delete_segment(self, file):
+        logger.debug(f"asked to delete segments {file}")
 
         segment_dir = os.path.join(self.output_dir, file)
         logger.debug(f"pruning {segment_dir}")
@@ -38,14 +48,41 @@ class CacheCleaner(object):
             shutil.rmtree(segment_dir)
         except FileNotFoundError:
             logger.debug(f"{segment_dir} not found")
-        self.db.delrecord(file)
+        self.segment_db.delrecord(file)
 
         # and try to remove the parent directories
         path = os.path.split(file)
         empty: bool = True
         logger.debug("cleaning empty parent directories")
         while empty and len(path[0]):
-            parent_dir = os.path.join(self.output_dir,path[0])
+            parent_dir = os.path.join(self.output_dir, path[0])
+            try:
+                logger.debug(f"rmdir {parent_dir}")
+                os.rmdir(parent_dir)
+            except OSError:
+                logger.debug(f"{parent_dir} not empty, done")
+                # directory not empty
+                empty = False
+
+            path = os.path.split(path[0])
+
+    def delete_input(self, file):
+        logger.debug(f"asked to delete input file {file}")
+
+        delete_file = os.path.join(self.input_dir, file)
+        logger.debug(f"removing {delete_file}")
+        try:
+            os.remove(delete_file)
+        except FileNotFoundError:
+            logger.debug(f"{delete_file} not found")
+        self.input_file_db.delrecord(file)
+
+        # and try to remove the parent directories
+        path = os.path.split(file)
+        empty: bool = True
+        logger.debug("cleaning empty parent directories")
+        while empty and len(path[0]):
+            parent_dir = os.path.join(self.input_dir, path[0])
             try:
                 logger.debug(f"rmdir {parent_dir}")
                 os.rmdir(parent_dir)
