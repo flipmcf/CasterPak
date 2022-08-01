b90c2c9dd48bdc772ffecc7edc9b3ab72732018f
flipmcf <flipmcf@gmail.com>
2022-08-01 11:39:15 -0400

    db context manager

--- cachedb.py
+++ cachedb.py
@@ -10,6 +10,20 @@ SEGMENT_FILE_CACHE = None
 INPUT_FILE_CACHE = 'inputfile'
 
 
+class SQLite(object):
+    def __init__(self, file='sqlite.db'):
+        self.file=file
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.file)
+        self.conn.row_factory = sqlite3.Row
+        return self.conn.cursor()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.commit()
+        self.conn.close()
+
+
 class CacheDB(object):
     def __init__(self, dbname: str = 'cacheDB.db', cache_name: str = None) -> None:
         """create a new instance of a CacheDB
@@ -17,10 +31,10 @@ class CacheDB(object):
            a 'cachetype' can be passed to differentiate different caches
         """
         self.table = None
-        self.db = sqlite3.connect(dbname,)
+        self.dbname = dbname
 
         if cache_name is None:
-            self.table = "default"
+            self.table = "default_table"
         else:
             self.table = ''.join(c for c in cache_name if c.isalnum())
             if self.table != cache_name:
@@ -30,8 +44,8 @@ class CacheDB(object):
                            filename text PRIMARY KEY,
                            timestamp int NOT NULL);
                          """
-
-        self.db.execute(create_query)
+        with SQLite(self.dbname) as cursor:
+            cursor.execute(create_query)
 
     def addrecord(self, filename: str = None, timestamp: str = None) -> None:
         if filename is None:
@@ -44,18 +58,20 @@ class CacheDB(object):
                     VALUES(?, ?)
                     ON CONFLICT(filename) DO UPDATE SET timestamp=excluded.timestamp
                  """
-        self.db.execute(query, (filename, timestamp))
+
+        with SQLite(self.dbname) as cursor:
+            cursor.execute(query, (filename, timestamp))
 
     def find(self, age_in_minutes: int) -> Iterable[str]:
-
-        cursor = self.db.cursor()
         then = int(datetime.datetime.now().timestamp()) - age_in_minutes*60
-        cursor.execute("SELECT filename FROM {self.table} WHERE timestamp < ?", (then,))
 
-        return [row[0] for row in cursor.fetchall()]
+        with SQLite(self.dbname) as cursor:
+            cursor.execute(f"SELECT filename FROM {self.table} WHERE timestamp < ?", (then,))
+            return [row[0] for row in cursor.fetchall()]
 
     def delrecord(self, filename: str) -> None:
-        cursor = self.db.cursor()
-        cursor.execute("DELETE FROM {self.table} WHERE filename == ?", (filename,))
+
+        with SQLite(self.dbname) as cursor:
+            cursor.execute("DELETE FROM {self.table} WHERE filename == ?", (filename,))
         
 
