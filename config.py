f14b39f346452e0a7b9f3bc4e0d7fb2c18a2b6b8
flipmcf <mcfaddenm@rfa.org>
2022-09-14 13:43:36 -0400

    Adding licence info

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+#Copyright (c) 2022, Michael McFadden & Radio Free Asia
+#BSD 3-Clause License
+#See file LICENCE or visit https://github.com/flipmcf/CasterPak/blob/master/LICENSE
 import os
 import configparser
 
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+#Copyright (c) 2022, Michael McFadden & Radio Free Asia
+#BSD 3-Clause License
+#See file LICENCE or visit https://github.com/flipmcf/CasterPak/blob/master/LICENSE
