6369cd447505ab95e25e96a9081ac48a0df378f1
flipmcf <mcfaddenm@rfa.org>
2022-09-14 13:27:25 -0400

    Add licence to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from distutils.core import setup
 
+from setuptools import setup
+from setuptools.command.egg_info import egg_info
+
+class egg_info_ex(egg_info):
+    """Includes license file into `.egg-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.egg-info` when building a distribution
+        if not self.distribution.have_run.get('install', True):
+            # `install` command is in progress, copy license
+            self.mkpath(self.egg_info)
+            self.copy_file('LICENSE', self.egg_info)
+
+        egg_info.run(self)
+
 setup(name='CasterPak',
       version='0.8',
       author="Michael McFadden (flipmcf)",
       author_email="flipmcf@gmail.com",
       py_modules=['casterpak'],
+      license_files = ('LICENSE',),
+      cmdclass = {'egg_info': egg_info_ex},
       install_requires=['flask',
                         '',
                         'requests',
