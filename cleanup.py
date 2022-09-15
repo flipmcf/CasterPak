fc5d0a6ec7aeb69a63f6f7a4a1c73ccaef181478
flipmcf <mcfaddenm@rfa.org>
2022-09-15 12:51:21 -0400

    #15 just framework for cache size thresholds

--- cleanup.py
+++ cleanup.py
@@ -21,9 +21,24 @@ logger.addHandler(fh)
 
 class CacheCleaner(object):
     def __init__(self):
-        self.output_dir = config['output']['segmentParentPath']
-        self.output_filename = config['output']['childManifestFilename']
-        self.input_dir = config['input']['videoCachePath']
+        output_config = config['output']
+        input_config = config['input']
+
+        try:
+            self.output_dir = output_config['segmentParentPath']
+        except KeyError:
+            logger.critical("No [output] segmentParentPath configured")
+
+        try:
+            self.input_dir = input_config['videoCachePath']
+        except KeyError:
+            logger.critical("No [input] videoCachePath configured")
+
+        self.segment_cache_size = output_config.getint('segment_file_cache_size', fallback=16384)
+        self.segment_cache_threshold = output_config.getint('segment_file_cache_limit', fallback=90)
+        self.input_cache_size = input_config.getint('input_file_cache_size', fallback=8192)
+        self.input_cache_threshold = input_config.getint('input_file_cache_limit', fallback=90)
+
         self.segment_db = cachedb.CacheDB(cache_name=cachedb.SEGMENT_FILE_CACHE)
         self.input_file_db = cachedb.CacheDB(cache_name=cachedb.INPUT_FILE_CACHE)
 
@@ -38,6 +53,16 @@ class CacheCleaner(object):
         for file in files:
             self.delete_input(file)
 
+        while self.segment_cache_threshold_reached:
+            logger.warning("segment cache threshold reached. consider increasing segment cache size")
+            file = self.get_oldest_segment_file()
+            self.delete_segment(file)
+
+        while self.input_cache_threshold_reached:
+            logger.warning("input file cache threshold reached. consider increasing input file cache size")
+            file = self.get_oldest_input_file()
+            self.delete_input(file)
+
         return 0
 
     def get_old_segment_files(self):
@@ -45,6 +70,12 @@ class CacheCleaner(object):
 
     def get_old_input_files(self):
         return self.input_file_db.find(int(config['cache']['input_file_age']))
+
+    def get_oldest_segment_file(self):
+        raise NotImplementedError
+
+    def get_oldest_input_file(self):
+        raise NotImplementedError
 
     def delete_segment(self, file):
         logger.debug(f"asked to delete segments {file}")
@@ -73,6 +104,14 @@ class CacheCleaner(object):
 
         parent_dir = os.path.dirname(file)
         self.clean_empty_parents(self.input_dir, parent_dir)
+
+    @property
+    def segment_cache_threshold_reached(self) -> bool:
+        return False
+
+    @property
+    def input_cache_threshold_reached(self) -> bool:
+        return False
 
     @staticmethod
     def clean_empty_parents(dir_root, sub_dir):
