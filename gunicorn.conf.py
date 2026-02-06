db72dfe517fa188dfeb5bd7ba3b2117177b43cc8
Michael McFadden <flipmcf@gmail.com>
2026-02-06 13:44:46 -0500

    change gunicorn umask

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,11 +7,9 @@ workers = 3
 bind = "unix:/home/casterpak/CasterPak/casterpak.sock"
 
 wsgi_app = "app:app"
-umask = 777  #A bit mask for the file mode on files written by Gunicorn.
+umask = 007  #A bit mask for the file mode on files written by Gunicorn.
 
 loglevel = 'debug'
-#accesslog = "/var/log/casterpak.access.log"
-#errorlog = "/var/log/casterpak.error.log"
 
 accesslog = app_config.get('logging', 'access_log', fallback='/var/log/casterpak.access.log')
 errorlog = app_config.get('logging', 'error_log', fallback='/var/log/casterpak.error.log')
