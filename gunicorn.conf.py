0c28916bb4ffbffb19cc15acf648763a0e913694
Michael McFadden <flipmcf@gmail.com>
2026-02-06 15:08:04 -0500

    fix umask and get ready for migration of cleanup from cron to thread

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,11 +7,18 @@ workers = 3
 bind = "unix:/home/casterpak/CasterPak/casterpak.sock"
 
 wsgi_app = "app:app"
-umask = 007  #A bit mask for the file mode on files written by Gunicorn.
+umask = 0x007  #Gunicorn creates files that are user & group writable.  Not world writable.
 
 loglevel = 'debug'
 
 accesslog = app_config.get('logging', 'access_log', fallback='/var/log/casterpak.access.log')
 errorlog = app_config.get('logging', 'error_log', fallback='/var/log/casterpak.error.log')
+cachelog = app_config.get('logging', 'cache_log', fallback='/var/log/casterpak.cache.log')
+
 
 capture_output = False
+
+# Server hook to initialize the cache cleanup thread.
+def on_starting(server):
+    # TODO create a thread to run the cleanup function
+    pass
