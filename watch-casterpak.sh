c361e57521cf7f88d60e4afe788da90e1958551d
Michael McFadden <flipmcf@gmail.com>
2026-02-20 13:38:28 -0500

    watch command now does input and segments separately

--- watch-casterpak.sh
+++ watch-casterpak.sh
@@ -7,8 +7,8 @@ docker exec -u 0 casterpak_server bash -c "apt-get update && apt-get install -y 
 # 2. CONFIG: Define our monitoring commands
 # We use 'bash -c' inside the tabs to keep them open if a command fails
 CMD_LOGS="docker-compose logs --tail=20 -f"
-CMD_FILES="docker exec -it casterpak_server watch -d -n 1 'tree -d -L 2 /tmp/video_input /tmp/segments'"
-
+CMD_INPUT_FILES="docker exec -it casterpak_server watch -d -n 1 'tree -s /tmp/video_input'"
+CMD_SEGMENTS="docker exec -it casterpak_server watch -d -n 1 'tree -d -L 2 /tmp/segments'"
 CMD_DB_INPUT='docker exec -it casterpak_server watch -n 1 "date +%s; sqlite3 /app/cacheDB.db '\''SELECT filename, (strftime(\"%s\", \"now\") - timestamp) || \"s\" AS age FROM inputfile ORDER BY timestamp DESC LIMIT 10;'\''"'
 
 CMD_DB_SEGMENT='docker exec -it casterpak_server watch -n 1 "date +%s; sqlite3 /app/cacheDB.db '\''SELECT filename, (strftime(\"%s\", \"now\") - timestamp) || \"s\" AS age FROM segmentfile ORDER BY timestamp DESC LIMIT 10;'\''"'
@@ -20,7 +20,8 @@ CMD_SHELL="docker exec -it casterpak_server /bin/bash"
 # 3. LAUNCH: windows 
 # If you prefer separate windows instead of tabs, remove the '--tab' flags
 gnome-terminal --window --title="HEARTBEAT: Logs" --geometry=80x24+0+0 -- bash -c "$CMD_LOGS" 
-gnome-terminal --window --title="Filesystem" --geometry=80x24+800+0 -- bash -c "$CMD_FILES"
+gnome-terminal --window --title="Filesystem" --geometry=80x24+800+0 -- bash -c "$CMD_INPUT_FILES"
+gnome-terminal --window --title="Filesystem" --geometry=80x24+800+0 -- bash -c "$CMD_SEGMENTS"
 gnome-terminal --window --title="DB: Input Files" --geometry=80x20+0+1550 -- bash -c "$CMD_DB_INPUT"
 gnome-terminal --window --title="DB: Segments" --geometry=80x20+800+1550 -- bash -c "$CMD_DB_SEGMENT"
 gnome-terminal --window --title="CHAOS: Manual Entry" --geometry=80x20+1600+1550 -- bash -c "$CMD_SHELL"
