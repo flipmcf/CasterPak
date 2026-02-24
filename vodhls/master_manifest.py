5c2a0a1c40a8b83814e4686f02fe722809ba5ab5
Michael McFadden <flipmcf@gmail.com>
2026-02-24 09:48:42 -0500

    Apple says that HLS segments should be 2 seconds long

--- vodhls/master_manifest.py
+++ vodhls/master_manifest.py
@@ -112,7 +112,7 @@ class MultivariantManager(object):
         try:
             return self.config['output']['segmentDuration']
         except KeyError:
-            return '6'
+            return '2'
 
     def output_hls(self):
         """ output the master manifest file.
